76e25c5a185dcb33b5930d33b0efc03a20de980c
joegamal <yusufmagamalth@gmail.com>
2026-06-12 07:33:30 +0300

    class syntax

---
+++ fclass.py
@@ -0,0 +1,22 @@
+
+#this is the class syntax
+class Person:
+    #static field
+    count = 0
+
+    #statis metod 
+    @staticmethod
+    def stat_meth(name):
+        print(f"name is {name}")
+    
+    #constructor 
+    def __init__(self):
+        print(f"this is constructor")
+        self.age = 21   
+    
+    #this makes a method looks like an attribute
+    @property
+    def age(self):
+        return self.age
+    
+    
